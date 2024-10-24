49028ed97f9d787a5190d997e3e1db09d1d9b2fd
Rohit Khadka <roheet@WagWan-MacBook-Air.local>
2024-10-24 15:55:12 +0545

    minor fixes

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app/models/categories_news_model.dart';
 import 'dart:convert';
@@ -5,9 +6,10 @@ import 'dart:convert';
 import 'package:news_app/models/news_headline_model.dart';
 
 class NewsRepository {
+  final apiKey = dotenv.env['NEWS_API_KEY'];
   Future<NewsHeadlinesModel> fetchNewsHeadlinesApi(String Source) async {
     String url =
-        "https://newsapi.org/v2/top-headlines?sources=$Source&apiKey=4a38da3af30f45d681ba4d6df61b0100";
+        "https://newsapi.org/v2/top-headlines?sources=$Source&apiKey=$apiKey";
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
@@ -18,8 +20,7 @@ class NewsRepository {
   }
 
   Future<CategoriesNewsModel> fetchCategoriesNewsApi(String category) async {
-    String url =
-        "https://newsapi.org/v2/everything?q=$category&apiKey=4a38da3af30f45d681ba4d6df61b0100";
+    String url = "https://newsapi.org/v2/everything?q=$category&apiKey=$apiKey";
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
